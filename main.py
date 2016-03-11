558338f705959cf2a9f241b8554f5522c5fcff33
Will Manson <willrtmanson@gmail.com>
2016-03-11 13:53:01 +0000

    Change brackets for Python 3

--- main.py
+++ main.py
@@ -11,31 +11,31 @@ import sys
 def testBoard():
     board = Board(fileName='test/board1')
     board.printBoard()
-    print ""
+    print("")
 
     col = 4
     height = 8
-    print "O piece at ", col, ",", height, " is True"
+    print("O piece at ", col, ",", height, " is True")
     assert board.isValidMove('o', height, col) == True
 
     col = 5
     height = 9
-    print "O piece at ", col, ",", height, " is False"
+    print("O piece at ", col, ",", height, " is False")
     assert board.isValidMove('o', height, col) == False
 
     col = 2
     height = 9
-    print "I piece at ", col, ",", height, " is True"
+    print("I piece at ", col, ",", height, " is True")
     assert board.isValidMove('i', height, col) == True
 
     col = 3
     height = 9
-    print "I piece at ", col, ",", height, " is False"
+    print("I piece at ", col, ",", height, " is False")
     assert board.isValidMove('i', height, col) == False
 
     col = 1
     height = 5
-    print "L piece at ", col, ",", height, " is False"
+    print("L piece at ", col, ",", height, " is False")
     assert board.isValidMove('l', height, col) == False
 
 #TODO write proper unit tests for the board class
@@ -49,9 +49,9 @@ def testRotation():
         p4 = board.rotatePiece(p3)
         for row in p1:
             for block in row:
-                print block,
-            print ""
-        print ""
+                print(block,
+            print("")
+        print("")
         assert p4 == p
 
 
